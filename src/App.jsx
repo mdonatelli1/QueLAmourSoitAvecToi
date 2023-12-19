e07e61fb2cc094eb94bda8b9dbe3d9211a7d5706
Samir B <smdae99@gmail.com>
2023-12-19 16:30:46 +0100

    Add restart button to display Welcome component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Welcome from './pages/Welcome.jsx'
+import FinalResultSuccess  from './pages/FinalResultSuccess.jsx';
 
 
 
 function App() {
   return (
     <>
-      <Welcome />
+      <FinalResultSuccess />
     </>
   )
 }
