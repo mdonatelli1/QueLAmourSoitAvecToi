c3ebc79ae9561f07c518bc21e9859e1a33ca0368
Paul-Lac <plnlacroix@gmail.com>
2023-12-19 15:03:11 +0100

    Add Q&A

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import Welcome from './pages/Welcome.jsx'
-import Date from './pages/Date.jsx'
 
 
 
 function App() {
   return (
     <>
-      <Date />
+      <Welcome />
     </>
   )
 }
