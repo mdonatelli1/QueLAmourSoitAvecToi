b5a5e3e8da2a5b65ef619c85c19b01e830264999
Samir B <smdae99@gmail.com>
2023-12-19 11:34:01 +0100

    Added condition on end result (success or fail)

--- src/pages/FinalResultSuccess.jsx
+++ src/pages/FinalResultSuccess.jsx
@@ -1,24 +1,26 @@
 import '../App.css'
 import './final_result.css'
-import Char from '../components/Char.jsx'
 import charList from '../data';
 
-function FinalResultSuccess () {
-  // Function a déclencher quand le date est raté (inutilisable ici, a déclencher sur un autre composant?):
-  /*function finalResultFailed() {
-    let failTitle = document.getElementById('final_result_h2')
-    let failParagraph = document.getElementById('final_result_paragraph')
-    failTitle.innerHTML = "L Bozo XD"
-    failParagraph.innerHTML = "Maybe next time ..."
-  }*/
+
+function FinalResultSuccess ({end}) {
+
   return (
-    <main>
+    <>
+    {end ? (<main>
       <section id='final_result'>
         <h2 id='final_result_h2' className='final_result_h2'>Lovely !</h2>
         <p id='final_result_paragraph'>Vous avez réussi, GG WP</p>
         <img className='final_result_img' src={charList[3].imageSRC/*Image partner*/}></img>
       </section>
-    </main>
+    </main>) : (<main>
+      <section id='final_result'>
+        <h2 id='final_result_h2' className='final_result_h2'>Fail</h2>
+        <p id='final_result_paragraph'>Vous avez perdu</p>
+        <img className='final_result_img' src={charList[3].imageSRC/*Image partner*/}></img>
+      </section>
+    </main>)}
+    </>
   )
 }
 
