da5cd3e37a99c6892efa7eb3e84926c16827d245
Paul-Lac <plnlacroix@gmail.com>
2023-12-19 14:57:40 +0100

    Add Q&A for 3 partners

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Welcome from './pages/Welcome.jsx'
+import Date from './pages/Date.jsx'
 
 
 
 function App() {
   return (
     <>
-      <Welcome />
+      <Date />
     </>
   )
 }
